7a4d9ce56e1011ca4bfed6d1c174d7abf7a0bfd7
sbegaudeau <stephane.begaudeau@gmail.com>
2012-09-20 14:10:56 +0200

    First version of the support of the configuration to parameterize the attributes of the newly created tasks.

--- org.eclipse.mylyn.tuleap.core/src/org/eclipse/mylyn/internal/tuleap/core/repository/TuleapTaskDataHandler.java
+++ org.eclipse.mylyn.tuleap.core/src/org/eclipse/mylyn/internal/tuleap/core/repository/TuleapTaskDataHandler.java
@@ -10,13 +10,21 @@
  *******************************************************************************/
 package org.eclipse.mylyn.internal.tuleap.core.repository;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.mylyn.internal.tuleap.core.client.TuleapClient;
+import org.eclipse.mylyn.internal.tuleap.core.model.AbstractTuleapField;
+import org.eclipse.mylyn.internal.tuleap.core.model.AbstractTuleapFormElement;
+import org.eclipse.mylyn.internal.tuleap.core.model.AbstractTuleapStructuralElement;
 import org.eclipse.mylyn.internal.tuleap.core.model.TuleapArtifact;
+import org.eclipse.mylyn.internal.tuleap.core.model.structural.TuleapFieldSet;
 import org.eclipse.mylyn.tasks.core.ITask;
 import org.eclipse.mylyn.tasks.core.ITaskMapping;
 import org.eclipse.mylyn.tasks.core.RepositoryResponse;
@@ -25,6 +33,7 @@ import org.eclipse.mylyn.tasks.core.TaskRepository;
 import org.eclipse.mylyn.tasks.core.data.AbstractTaskDataHandler;
 import org.eclipse.mylyn.tasks.core.data.TaskAttribute;
 import org.eclipse.mylyn.tasks.core.data.TaskAttributeMapper;
+import org.eclipse.mylyn.tasks.core.data.TaskAttributeMetaData;
 import org.eclipse.mylyn.tasks.core.data.TaskData;
 import org.eclipse.mylyn.tasks.core.data.TaskDataCollector;
 
@@ -146,6 +155,120 @@ public class TuleapTaskDataHandler extends AbstractTaskDataHandler {
 		TuleapRepositoryConfiguration configuration = tuleapClient.getRepositoryConfiguration();
 		if (configuration != null) {
 			// TODO Configure content to display it in the editor later
+
+			// Assigned to
+			TaskAttribute attribute = taskData.getRoot().createAttribute(TaskAttribute.USER_ASSIGNED);
+			TaskAttributeMetaData metaData = attribute.getMetaData();
+			metaData.setLabel("Assigned to");
+			metaData.setKind(TaskAttribute.KIND_DEFAULT);
+			metaData.setType(TaskAttribute.TYPE_PERSON);
+			attribute.setValue("stephane.begaudeau@gmail.com");
+
+			// Priority
+			attribute = taskData.getRoot().createAttribute(TaskAttribute.PRIORITY);
+			metaData = attribute.getMetaData();
+			metaData.setLabel("Priority");
+			metaData.setKind(TaskAttribute.KIND_DEFAULT);
+			metaData.setType(TaskAttribute.TYPE_SINGLE_SELECT);
+			attribute.putOption("P1", "P1");
+			attribute.putOption("P2", "P2");
+			attribute.putOption("P3", "P3");
+			attribute.putOption("P4", "P4");
+			attribute.putOption("P5", "P5");
+			attribute.setValue("P3");
+
+			// Summary
+			attribute = taskData.getRoot().createAttribute(TaskAttribute.SUMMARY);
+			metaData = attribute.getMetaData();
+			metaData.setLabel("Summary");
+			metaData.setKind(TaskAttribute.KIND_DEFAULT);
+			metaData.setType(TaskAttribute.TYPE_SHORT_RICH_TEXT);
+			attribute.setValue("Basic summary of a new Tuleap task");
+
+			// Creation date
+			attribute = taskData.getRoot().createAttribute(TaskAttribute.DATE_CREATION);
+			metaData = attribute.getMetaData();
+			metaData.setLabel("Created");
+			metaData.setKind(TaskAttribute.KIND_DEFAULT);
+			metaData.setType(TaskAttribute.TYPE_DATE);
+			attribute.setValue("");
+
+			// Last modification date
+			attribute = taskData.getRoot().createAttribute(TaskAttribute.DATE_MODIFICATION);
+			metaData = attribute.getMetaData();
+			metaData.setLabel("Modified");
+			metaData.setKind(TaskAttribute.KIND_DEFAULT);
+			metaData.setType(TaskAttribute.TYPE_DATE);
+			attribute.setValue("");
+
+			// Resolution
+			attribute = taskData.getRoot().createAttribute(TaskAttribute.RESOLUTION);
+			metaData = attribute.getMetaData();
+			metaData.setLabel("Resolution");
+			metaData.setKind(TaskAttribute.KIND_DEFAULT);
+			metaData.setType(TaskAttribute.TYPE_SINGLE_SELECT);
+			attribute.putOption("NEW", "NEW");
+			attribute.putOption("ASSIGNED", "ASSIGNED");
+			attribute.putOption("SOLVED", "SOLVED");
+			attribute.putOption("INVALID", "INVALID");
+			attribute.setValue("NEW");
+
+			// Default attributes
+			List<AbstractTuleapStructuralElement> formElements = configuration.getFormElements();
+			for (AbstractTuleapStructuralElement abstractTuleapStructuralElement : formElements) {
+				if (abstractTuleapStructuralElement instanceof TuleapFieldSet) {
+					TuleapFieldSet tuleapFieldSet = (TuleapFieldSet)abstractTuleapStructuralElement;
+					List<AbstractTuleapFormElement> fieldSetFormElements = tuleapFieldSet.getFormElements();
+					for (AbstractTuleapFormElement abstractTuleapFormElement : fieldSetFormElements) {
+						if (abstractTuleapFormElement instanceof AbstractTuleapField) {
+							this.createAttribute(taskData, (AbstractTuleapField)abstractTuleapFormElement);
+						}
+					}
+				}
+			}
+
+			// Additional properties
+
+		}
+	}
+
+	/**
+	 * Creates the attribute representing the Tuleap field in the given Mylyn task data.
+	 * 
+	 * @param taskData
+	 *            The Mylyn task data
+	 * @param tuleapField
+	 *            The Tuleap field
+	 */
+	private void createAttribute(TaskData taskData, AbstractTuleapField tuleapField) {
+		TaskAttribute attribute = taskData.getRoot().createAttribute(tuleapField.getIdentifier());
+
+		TaskAttributeMetaData attributeMetadata = attribute.getMetaData();
+		attributeMetadata.setType(tuleapField.getMetadataType());
+		attributeMetadata.setKind(tuleapField.getMetadataKind());
+		// TODO Support advanced permissions (read only / visible)
+		// attributeMetadata.setReadOnly(tuleaField)
+		attributeMetadata.setLabel(tuleapField.getLabel());
+
+		Map<String, String> options = tuleapField.getOptions();
+		for (Entry<String, String> entry : options.entrySet()) {
+			attribute.putOption(entry.getKey(), entry.getValue());
+		}
+
+		Object defaultValue = tuleapField.getDefaultValue();
+		if (defaultValue instanceof String) {
+			attribute.setValue((String)defaultValue);
+		} else if (defaultValue instanceof List<?>) {
+			List<?> list = (List<?>)defaultValue;
+
+			List<String> strList = new ArrayList<String>();
+			for (Object object : list) {
+				if (object instanceof String) {
+					strList.add((String)object);
+				}
+			}
+
+			attribute.setValues(strList);
 		}
 	}
 
