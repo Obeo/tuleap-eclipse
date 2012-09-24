d8e4c442c004af992c84d7af1520b7aa8172cbe7
sbegaudeau <stephane.begaudeau@gmail.com>
2012-09-24 11:49:24 +0200

    Fixing a comment.

--- org.eclipse.mylyn.tuleap.ui/src/org/eclipse/mylyn/internal/tuleap/ui/repository/TuleapConnectorUi.java
+++ org.eclipse.mylyn.tuleap.ui/src/org/eclipse/mylyn/internal/tuleap/ui/repository/TuleapConnectorUi.java
@@ -45,13 +45,13 @@ public class TuleapConnectorUi extends AbstractRepositoryConnectorUi {
 
 	@Override
 	public String getAccountCreationUrl(TaskRepository taskRepository) {
-		// Account creation url : https://<nom-de-domaine>/account/register.php
+		// Account creation url : https://<domain-name>/account/register.php
 		return TuleapUtil.getDomainRepositoryURl(taskRepository) + "/account/register.php"; //$NON-NLS-1$
 	}
 
 	@Override
 	public String getAccountManagementUrl(TaskRepository taskRepository) {
-		// Account management url : https://<nom-de-domaine>/my/
+		// Account management url : https://<domain-name>/my/
 		return TuleapUtil.getDomainRepositoryURl(taskRepository) + "/my/"; //$NON-NLS-1$
 	}
 
