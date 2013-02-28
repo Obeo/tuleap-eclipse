3053ece96680945844626f37c5af9e3844d09b53
Mélanie Bats <melanie.bats@obeo.fr>
2013-02-28 16:30:25 +0100

    Add tests on TuleapSoapConnector
    Test on :
    - Getting the configuration
    - Getting an artifact
    - Updating an artifact
    - Getting the reports

---
+++ org.eclipse.mylyn.tuleap.tests/src/org/eclipse/mylyn/tuleap/tests/core/MockedAbstractWebLocation.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylyn.tuleap.tests.core;
+
+import java.net.Proxy;
+
+import org.eclipse.mylyn.commons.net.AbstractWebLocation;
+import org.eclipse.mylyn.commons.net.AuthenticationCredentials;
+import org.eclipse.mylyn.commons.net.AuthenticationType;
+
+/**
+ * A mock of an abstract web location.
+ * 
+ * @author <a href="mailto:melanie.bats@obeo.fr">Melanie Bats</a>
+ * @since 0.7
+ */
+public class MockedAbstractWebLocation extends AbstractWebLocation {
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param url
+	 *            URL
+	 */
+	public MockedAbstractWebLocation(String url) {
+		super(url);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.mylyn.commons.net.AbstractWebLocation#getCredentials(org.eclipse.mylyn.commons.net.AuthenticationType)
+	 */
+	@Override
+	public AuthenticationCredentials getCredentials(AuthenticationType type) {
+		return new AuthenticationCredentials("userName", "password"); //$NON-NLS-1$//$NON-NLS-2$
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.mylyn.commons.net.AbstractWebLocation#getProxyForHost(java.lang.String,
+	 *      java.lang.String)
+	 */
+	@Override
+	public Proxy getProxyForHost(String host, String proxyType) {
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ org.eclipse.mylyn.tuleap.tests/src/org/eclipse/mylyn/tuleap/tests/core/MockedTracker.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylyn.tuleap.tests.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.mylyn.internal.tuleap.core.wsdl.soap.v2.Tracker;
+import org.eclipse.mylyn.internal.tuleap.core.wsdl.soap.v2.TrackerField;
+
+/**
+ * A parameterizable tracker.
+ * 
+ * @author <a href="mailto:melanie.bats@obeo.fr">Melanie Bats</a>
+ * @since 0.7
+ */
+public class MockedTracker extends Tracker {
+
+	/**
+	 * Serial Version UID.
+	 */
+	private static final long serialVersionUID = -8496198657805355320L;
+
+	/**
+	 * Tracker fields.
+	 */
+	private TrackerField[] trackerFields;
+
+	/**
+	 * Artifacts defined in current tracker.
+	 */
+	private Map<Integer, MockedArtifact> artifacts = new HashMap<Integer, MockedArtifact>();
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param trackerId
+	 *            Tracker ID
+	 * @param name
+	 *            Tracker name
+	 * @param fields
+	 *            Tracker fields
+	 */
+	public MockedTracker(int trackerId, String name, TrackerField[] fields) {
+		super(trackerId, 42, name, "description", "itemName"); //$NON-NLS-1$//$NON-NLS-2$
+		this.trackerFields = fields;
+	}
+
+	/**
+	 * Get the tracker fields.
+	 * 
+	 * @return The tracker fields
+	 */
+	public TrackerField[] getTrackerFields() {
+		return trackerFields;
+	}
+
+	/**
+	 * Get artifact.
+	 * 
+	 * @param artifactId
+	 *            Artifact ID
+	 * @return Artifact
+	 */
+	public MockedArtifact getArtifact(int artifactId) {
+		return artifacts.get(Integer.valueOf(artifactId));
+	}
+
+	/**
+	 * Set artifact.
+	 * 
+	 * @param artifact
+	 *            Artifact
+	 */
+	public void setArtifact(MockedArtifact artifact) {
+		artifacts.put(Integer.valueOf(artifact.getArtifact_id()), artifact);
+	}
+}
