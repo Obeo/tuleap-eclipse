eabe4ccd9dc93ef1f9eceba4dafd626f9556c95e
fbacha <fbacha@obeo.fr>
2013-08-14 10:23:29 +0200

    improving tests on the parent/child trackers relationship

--- org.tuleap.mylyn.task.tests/src/org/tuleap/mylyn/task/internal/tests/model/TuleapTrackerConfigurationTests.java
+++ org.tuleap.mylyn.task.tests/src/org/tuleap/mylyn/task/internal/tests/model/TuleapTrackerConfigurationTests.java
@@ -59,6 +59,21 @@ public class TuleapTrackerConfigurationTests {
 	private TuleapTrackerConfiguration tracker6;
 
 	/**
+	 * The seventh tracker.
+	 */
+	private TuleapTrackerConfiguration tracker7;
+
+	/**
+	 * The eighth tracker.
+	 */
+	private TuleapTrackerConfiguration tracker8;
+
+	/**
+	 * The ninth tracker.
+	 */
+	private TuleapTrackerConfiguration tracker9;
+
+	/**
 	 * The first url.
 	 */
 	private String firstUrl = "localhost:3001/api/v3.14/trackers/1"; //$NON-NLS-1$
@@ -89,6 +104,21 @@ public class TuleapTrackerConfigurationTests {
 	private String sixthUrl = "localhost:3001/api/v3.14/trackers/6"; //$NON-NLS-1$
 
 	/**
+	 * The sevent url.
+	 */
+	private String seventhUrl = "localhost:3001/api/v3.14/trackers/7"; //$NON-NLS-1$
+
+	/**
+	 * The sixth url.
+	 */
+	private String eighthUrl = "localhost:3001/api/v3.14/trackers/8"; //$NON-NLS-1$
+
+	/**
+	 * The sixth url.
+	 */
+	private String ninthUrl = "localhost:3001/api/v3.14/trackers/9"; //$NON-NLS-1$
+
+	/**
 	 * Set up the test.
 	 */
 	@Before
@@ -100,6 +130,9 @@ public class TuleapTrackerConfigurationTests {
 		tracker4 = new TuleapTrackerConfiguration(4, fourthUrl);
 		tracker5 = new TuleapTrackerConfiguration(5, fifthUrl);
 		tracker6 = new TuleapTrackerConfiguration(6, sixthUrl);
+		tracker7 = new TuleapTrackerConfiguration(7, seventhUrl);
+		tracker8 = new TuleapTrackerConfiguration(8, eighthUrl);
+		tracker9 = new TuleapTrackerConfiguration(9, ninthUrl);
 
 	}
 
@@ -119,14 +152,22 @@ public class TuleapTrackerConfigurationTests {
 		tracker1.addChildTracker(tracker5);
 		tracker1.addChildTracker(tracker6);
 
-		// testing the children tracker collection
+		// testing that the parent contains all children
+
 		Collection<TuleapTrackerConfiguration> children = tracker1.getChildrenTrackers();
 		assertEquals(5, children.size());
 		assertTrue(children.contains(tracker2));
 		assertTrue(children.contains(tracker3));
-		assertEquals(1, tracker4.getParentTracker().getTrackerId());
-		assertEquals(1, tracker5.getParentTracker().getTrackerId());
-		assertEquals(1, tracker6.getParentTracker().getTrackerId());
+		assertTrue(children.contains(tracker4));
+		assertTrue(children.contains(tracker5));
+		assertTrue(children.contains(tracker6));
+
+		// testing that tracker 1 is the parent of trackers 2, 3, 4, 5 and 6
+		assertEquals(tracker1, tracker2.getParentTracker());
+		assertEquals(tracker1, tracker3.getParentTracker());
+		assertEquals(tracker1, tracker4.getParentTracker());
+		assertEquals(tracker1, tracker5.getParentTracker());
+		assertEquals(tracker1, tracker6.getParentTracker());
 
 		// test changing the parent
 		tracker5.setParentTracker(tracker3);
@@ -136,6 +177,84 @@ public class TuleapTrackerConfigurationTests {
 
 		assertEquals(4, children.size());
 		assertFalse(children.contains(tracker5));
+
+		// changing the parent using setParent
+
+		tracker8.setParentTracker(tracker7);
+		assertEquals(tracker7, tracker8.getParentTracker());
+		assertTrue(tracker7.getChildrenTrackers().contains(tracker8));
+
+		tracker8.setParentTracker(tracker9);
+		assertEquals(tracker9, tracker8.getParentTracker());
+		assertTrue(tracker9.getChildrenTrackers().contains(tracker8));
+		assertFalse(tracker7.getChildrenTrackers().contains(tracker8));
+
+		// changing the parent using addChild
+
+		tracker7.addChildTracker(tracker8);
+		assertEquals(tracker7, tracker8.getParentTracker());
+		assertTrue(tracker7.getChildrenTrackers().contains(tracker8));
+
+		tracker9.addChildTracker(tracker8);
+		assertEquals(tracker9, tracker8.getParentTracker());
+		assertTrue(tracker9.getChildrenTrackers().contains(tracker8));
+		assertFalse(tracker7.getChildrenTrackers().contains(tracker8));
+
+		// adding a tracker to its self as a child
+		tracker6.addChildTracker(tracker6);
+		assertEquals(0, tracker6.getChildrenTrackers().size());
+
+		// adding a tracker to its self as a parent
+		tracker4.setParentTracker(tracker4);
+		assertEquals(1, tracker4.getParentTracker().getTrackerId());
+
 	}
 
+	/**
+	 * Testing changing the parent using setParent method.
+	 */
+	@Test
+	public void testChangingParentTrackerUsingSetParent() {
+
+		tracker8.setParentTracker(tracker7);
+		assertEquals(tracker7, tracker8.getParentTracker());
+		assertTrue(tracker7.getChildrenTrackers().contains(tracker8));
+
+		tracker8.setParentTracker(tracker9);
+		assertEquals(tracker9, tracker8.getParentTracker());
+		assertTrue(tracker9.getChildrenTrackers().contains(tracker8));
+		assertFalse(tracker7.getChildrenTrackers().contains(tracker8));
+	}
+
+	/**
+	 * Testing changing the parent using addChild method.
+	 */
+	@Test
+	public void testParentChildrenTrackersUsingAddChild() {
+
+		tracker7.addChildTracker(tracker8);
+		assertEquals(tracker7, tracker8.getParentTracker());
+		assertTrue(tracker7.getChildrenTrackers().contains(tracker8));
+
+		tracker9.addChildTracker(tracker8);
+		assertEquals(tracker9, tracker8.getParentTracker());
+		assertTrue(tracker9.getChildrenTrackers().contains(tracker8));
+		assertFalse(tracker7.getChildrenTrackers().contains(tracker8));
+	}
+
+	/**
+	 * Testing irregular cases .
+	 */
+	@Test
+	public void testIrrigularCases() {
+
+		// adding a tracker to its self as a child
+		tracker6.addChildTracker(tracker6);
+		assertEquals(0, tracker6.getChildrenTrackers().size());
+
+		// adding a tracker to its self as a parent
+		tracker4.setParentTracker(tracker1);
+		tracker4.setParentTracker(tracker4);
+		assertEquals(tracker1, tracker4.getParentTracker());
+	}
 }
