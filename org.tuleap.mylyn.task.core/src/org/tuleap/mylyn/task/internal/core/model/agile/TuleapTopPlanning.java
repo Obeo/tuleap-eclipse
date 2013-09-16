2031546ec58ed46bf1db11422ae3271cf40b9ad2
Laurent Delaigue <laurent.delaigue@obeo.fr>
2013-09-16 14:32:56 +0200

    TopPlanning POJO.

---
+++ org.tuleap.mylyn.task.core/src/org/tuleap/mylyn/task/internal/core/model/agile/TuleapTopPlanning.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.tuleap.mylyn.task.internal.core.model.agile;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * This POJO represents a top planning.
+ * 
+ * @author <a href="mailto:laurent.delaigue@obeo.fr">Laurent Delaigue</a>
+ */
+public class TuleapTopPlanning extends AbstractTuleapAgileElement {
+
+	/**
+	 * The top planning's milestones.
+	 */
+	protected final List<TuleapMilestone> milestones = Lists.newArrayList();
+
+	/**
+	 * The top planning's backlog items.
+	 */
+	protected final List<TuleapBacklogItem> backlogItems = Lists.newArrayList();
+
+	/**
+	 * Provides an immutable view of the top planning's milestones.
+	 * 
+	 * @return the milestones
+	 */
+	public ImmutableList<TuleapMilestone> getMilestones() {
+		return ImmutableList.copyOf(milestones);
+	}
+
+	/**
+	 * Provides an immutable view of the top planning's backlog items.
+	 * 
+	 * @return the backlogItems
+	 */
+	public ImmutableList<TuleapBacklogItem> getBacklogItems() {
+		return ImmutableList.copyOf(backlogItems);
+	}
+
+	/**
+	 * Adds the given milestone to the top planning's list of milestones.
+	 * 
+	 * @param milestone
+	 *            The milestone to add.
+	 */
+	public void addMilestone(TuleapMilestone milestone) {
+		this.milestones.add(milestone);
+	}
+
+	/**
+	 * Adds the given backlog item to the top planning's list of backlog items.
+	 * 
+	 * @param backlogItem
+	 *            The backlog item to add.
+	 */
+	public void addBacklogItem(TuleapBacklogItem backlogItem) {
+		this.backlogItems.add(backlogItem);
+	}
+}
