0f6a71139cd2d172672d1114c8768d0426c76e71
Laurent Delaigue <laurent.delaigue@obeo.fr>
2013-09-26 13:58:03 +0200

    Fixed TuleapArtifactTests.

--- org.tuleap.mylyn.task.tests/src/org/tuleap/mylyn/task/internal/tests/client/TuleapArtifactTests.java
+++ org.tuleap.mylyn.task.tests/src/org/tuleap/mylyn/task/internal/tests/client/TuleapArtifactTests.java
@@ -11,16 +11,21 @@
 package org.tuleap.mylyn.task.internal.tests.client;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Test;
 import org.tuleap.mylyn.task.internal.core.data.AbstractFieldValue;
+import org.tuleap.mylyn.task.internal.core.data.AttachmentFieldValue;
+import org.tuleap.mylyn.task.internal.core.data.AttachmentValue;
 import org.tuleap.mylyn.task.internal.core.data.BoundFieldValue;
 import org.tuleap.mylyn.task.internal.core.data.LiteralFieldValue;
 import org.tuleap.mylyn.task.internal.core.model.AbstractTuleapConfigurableElement;
+import org.tuleap.mylyn.task.internal.core.model.TuleapPerson;
 import org.tuleap.mylyn.task.internal.core.model.tracker.TuleapArtifact;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 /**
  * This class will test Tuleap artifacts.
@@ -46,7 +51,6 @@ public class TuleapArtifactTests {
 		artifact.addFieldValue(value0);
 		AbstractFieldValue fieldValue = artifact.getFieldValue(123);
 		assertEquals(123, fieldValue.getFieldId());
-		assertSame(fieldValue, value0);
 		assertEquals(lbl, ((LiteralFieldValue)fieldValue).getFieldValue());
 	}
 
@@ -60,7 +64,35 @@ public class TuleapArtifactTests {
 		artifact.addFieldValue(value0);
 		AbstractFieldValue fieldValue = artifact.getFieldValue(123);
 		assertEquals(123, fieldValue.getFieldId());
-		assertSame(fieldValue, value0);
-		assertEquals("Some value", ((LiteralFieldValue)fieldValue).getFieldValue()); //$NON-NLS-1$
+		int i = 0;
+		for (Integer boundValue : ((BoundFieldValue)value0).getValueIds()) {
+			assertEquals(i++, boundValue.intValue());
+		}
+	}
+
+	/**
+	 * This test will ensure that once the value of a Tuleap attribute is set with a bound value, we can
+	 * access it.
+	 */
+	@Test
+	public void testAddAttachmentFieldValue() {
+		AttachmentValue attVal = new AttachmentValue("111", "The name", new TuleapPerson("ldelaigue", //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
+				"Laurent Delaigue", 555, "laurent.delaigue@some.com"), 123, "Description", "text/html"); //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		AttachmentFieldValue value0 = new AttachmentFieldValue(123, Collections.singletonList(attVal));
+		artifact.addFieldValue(value0);
+		AbstractFieldValue fieldValue = artifact.getFieldValue(123);
+		assertEquals(123, fieldValue.getFieldId());
+		assertTrue(fieldValue instanceof AttachmentFieldValue);
+		List<AttachmentValue> attList = ((AttachmentFieldValue)fieldValue).getAttachments();
+		assertEquals(1, attList.size());
+		AttachmentValue att = attList.get(0);
+		assertEquals("111", att.getAttachmentId()); //$NON-NLS-1$
+		assertEquals("text/html", att.getContentType()); //$NON-NLS-1$
+		assertEquals("Description", att.getDescription()); //$NON-NLS-1$
+		assertEquals("The name", att.getFilename()); //$NON-NLS-1$
+		assertEquals(555, att.getPerson().getId());
+		assertEquals("ldelaigue", att.getPerson().getUserName()); //$NON-NLS-1$
+		assertEquals("Laurent Delaigue", att.getPerson().getRealName()); //$NON-NLS-1$
+		assertEquals("laurent.delaigue@some.com", att.getPerson().getEmail()); //$NON-NLS-1$
 	}
 }
