0e442d192895b4d6bca73ea963b2c2292d9f4962
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2013-10-03 16:11:25 +0200

    Add new unit tests of the TuleapRepositoryConnector.

--- org.tuleap.mylyn.task.tests/src/org/tuleap/mylyn/task/internal/tests/repository/TuleapRepositoryConnectorTests.java
+++ org.tuleap.mylyn.task.tests/src/org/tuleap/mylyn/task/internal/tests/repository/TuleapRepositoryConnectorTests.java
@@ -10,9 +10,36 @@
  *******************************************************************************/
 package org.tuleap.mylyn.task.internal.tests.repository;
 
+import com.google.common.collect.Lists;
+
+import java.util.Date;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.mylyn.internal.tasks.core.LocalTask;
+import org.eclipse.mylyn.internal.tasks.core.RepositoryQuery;
+import org.eclipse.mylyn.tasks.core.IRepositoryQuery;
+import org.eclipse.mylyn.tasks.core.ITask;
+import org.eclipse.mylyn.tasks.core.TaskRepository;
+import org.eclipse.mylyn.tasks.core.data.TaskAttributeMapper;
+import org.eclipse.mylyn.tasks.core.data.TaskData;
+import org.eclipse.mylyn.tasks.core.data.TaskMapper;
 import org.junit.Test;
+import org.tuleap.mylyn.task.internal.core.client.ITuleapQueryConstants;
+import org.tuleap.mylyn.task.internal.core.client.TuleapClientManager;
+import org.tuleap.mylyn.task.internal.core.client.soap.TuleapSoapClient;
 import org.tuleap.mylyn.task.internal.core.data.TuleapTaskIdentityUtil;
+import org.tuleap.mylyn.task.internal.core.model.TuleapProjectConfiguration;
+import org.tuleap.mylyn.task.internal.core.model.TuleapServerConfiguration;
+import org.tuleap.mylyn.task.internal.core.model.tracker.TuleapArtifact;
+import org.tuleap.mylyn.task.internal.core.model.tracker.TuleapTrackerConfiguration;
 import org.tuleap.mylyn.task.internal.core.repository.TuleapRepositoryConnector;
+import org.tuleap.mylyn.task.internal.core.repository.TuleapTaskDataCollector;
+import org.tuleap.mylyn.task.internal.core.util.ITuleapConstants;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 import static org.junit.Assert.assertEquals;
 
@@ -61,4 +88,238 @@ public class TuleapRepositoryConnectorTests {
 		assertEquals("https://tuleap.net/plugins/tracker/?group_id=409&tracker=31&aid=821", taskUrl); //$NON-NLS-1$
 	}
 
+	/**
+	 * Test if the task has changed.
+	 */
+	@Test
+	public void testHasTaskChanged() {
+		TuleapRepositoryConnector tuleapRepositoryConnector = new TuleapRepositoryConnector();
+
+		Date dateModified = new Date();
+
+		ITask task = new LocalTask("https://tuleap.net", "123:456#789");
+		task.setModificationDate(dateModified);
+
+		TaskData taskData = new TaskData(new TaskAttributeMapper(new TaskRepository(
+				ITuleapConstants.CONNECTOR_KIND, "https://tuleap.net")), ITuleapConstants.CONNECTOR_KIND,
+				"https://tuleap.net", "123:456#789");
+
+		TaskMapper mapper = new TaskMapper(taskData, true);
+		mapper.setModificationDate(dateModified);
+
+		boolean hasTaskChanged = tuleapRepositoryConnector.hasTaskChanged(null, task, taskData);
+		assertThat(hasTaskChanged, is(false));
+
+		TaskData taskData2 = new TaskData(new TaskAttributeMapper(new TaskRepository(
+				ITuleapConstants.CONNECTOR_KIND, "https://tuleap.net")), ITuleapConstants.CONNECTOR_KIND,
+				"https://tuleap.net", "123:456#789");
+
+		mapper = new TaskMapper(taskData, true);
+		mapper.setModificationDate(new Date());
+
+		hasTaskChanged = tuleapRepositoryConnector.hasTaskChanged(null, task, taskData2);
+		assertThat(hasTaskChanged, is(true));
+	}
+
+	/**
+	 * Test the execution of a query to retrieve all the artifacts from a given tracker.
+	 */
+	@Test
+	public void testPerformQueryAllFromTracker() {
+		int projectId = 979;
+		int trackerId = 42;
+		int artifactId = 121;
+
+		final TuleapServerConfiguration tuleapServerConfiguration = new TuleapServerConfiguration(
+				"https://tuleap.net");
+
+		TuleapProjectConfiguration tuleapProjectConfiguration = new TuleapProjectConfiguration("", projectId);
+		TuleapTrackerConfiguration trackerConfiguration = new TuleapTrackerConfiguration(trackerId, null,
+				null, null, null, 0);
+		tuleapProjectConfiguration.addTracker(trackerConfiguration);
+
+		tuleapServerConfiguration.addProject(tuleapProjectConfiguration);
+
+		final TuleapArtifact tuleapArtifact = new TuleapArtifact(artifactId, trackerId, "", "", "",
+				new Date(), new Date());
+
+		final TuleapClientManager tuleapClientManager = new TuleapClientManager() {
+			@Override
+			public TuleapSoapClient getSoapClient(TaskRepository taskRepository) {
+				return new TuleapSoapClient(null, null, null, null, null) {
+					@Override
+					public List<TuleapArtifact> getArtifactsFromQuery(IRepositoryQuery query,
+							TuleapServerConfiguration serverConfiguration,
+							TuleapTrackerConfiguration tuleapTrackerConfiguration, IProgressMonitor monitor) {
+						return Lists.newArrayList(tuleapArtifact);
+					}
+				};
+			}
+		};
+
+		TuleapRepositoryConnector tuleapRepositoryConnector = new TuleapRepositoryConnector() {
+			@Override
+			public TuleapServerConfiguration getRepositoryConfiguration(TaskRepository taskRepository,
+					boolean forceRefresh, IProgressMonitor monitor) {
+				return tuleapServerConfiguration;
+			}
+
+			@Override
+			public TuleapClientManager getClientManager() {
+				return tuleapClientManager;
+			}
+		};
+
+		TaskRepository taskRepository = new TaskRepository(ITuleapConstants.CONNECTOR_KIND,
+				"https://tuleap.net");
+
+		// All from tracker
+		TuleapTaskDataCollector collector = new TuleapTaskDataCollector();
+
+		IRepositoryQuery query = new RepositoryQuery(ITuleapConstants.CONNECTOR_KIND, "");
+		query.setAttribute(ITuleapQueryConstants.QUERY_KIND,
+				ITuleapQueryConstants.QUERY_KIND_ALL_FROM_TRACKER);
+		query.setAttribute(ITuleapQueryConstants.QUERY_CONFIGURATION_ID, String.valueOf(trackerId));
+
+		tuleapRepositoryConnector.performQuery(taskRepository, query, collector, null,
+				new NullProgressMonitor());
+
+		assertThat(collector.getTaskData().size(), is(1));
+		assertThat(collector.getTaskData().iterator().next().getTaskId(), is(TuleapTaskIdentityUtil
+				.getTaskDataId(projectId, trackerId, artifactId)));
+	}
+
+	/**
+	 * Test the execution of a query to retrieve all the artifacts from a given tracker.
+	 */
+	@Test
+	public void testPerformQueryReport() {
+		int projectId = 979;
+		int trackerId = 42;
+		int artifactId = 121;
+
+		final TuleapServerConfiguration tuleapServerConfiguration = new TuleapServerConfiguration(
+				"https://tuleap.net");
+
+		TuleapProjectConfiguration tuleapProjectConfiguration = new TuleapProjectConfiguration("", projectId);
+		TuleapTrackerConfiguration trackerConfiguration = new TuleapTrackerConfiguration(trackerId, null,
+				null, null, null, 0);
+		tuleapProjectConfiguration.addTracker(trackerConfiguration);
+
+		tuleapServerConfiguration.addProject(tuleapProjectConfiguration);
+
+		final TuleapArtifact tuleapArtifact = new TuleapArtifact(artifactId, trackerId, "", "", "",
+				new Date(), new Date());
+
+		final TuleapClientManager tuleapClientManager = new TuleapClientManager() {
+			@Override
+			public TuleapSoapClient getSoapClient(TaskRepository taskRepository) {
+				return new TuleapSoapClient(null, null, null, null, null) {
+					@Override
+					public List<TuleapArtifact> getArtifactsFromQuery(IRepositoryQuery query,
+							TuleapServerConfiguration serverConfiguration,
+							TuleapTrackerConfiguration tuleapTrackerConfiguration, IProgressMonitor monitor) {
+						return Lists.newArrayList(tuleapArtifact);
+					}
+				};
+			}
+		};
+
+		TuleapRepositoryConnector tuleapRepositoryConnector = new TuleapRepositoryConnector() {
+			@Override
+			public TuleapServerConfiguration getRepositoryConfiguration(TaskRepository taskRepository,
+					boolean forceRefresh, IProgressMonitor monitor) {
+				return tuleapServerConfiguration;
+			}
+
+			@Override
+			public TuleapClientManager getClientManager() {
+				return tuleapClientManager;
+			}
+		};
+
+		TaskRepository taskRepository = new TaskRepository(ITuleapConstants.CONNECTOR_KIND,
+				"https://tuleap.net");
+
+		// All from tracker
+		TuleapTaskDataCollector collector = new TuleapTaskDataCollector();
+
+		IRepositoryQuery query = new RepositoryQuery(ITuleapConstants.CONNECTOR_KIND, "");
+		query.setAttribute(ITuleapQueryConstants.QUERY_KIND, ITuleapQueryConstants.QUERY_KIND_REPORT);
+		query.setAttribute(ITuleapQueryConstants.QUERY_CONFIGURATION_ID, String.valueOf(trackerId));
+
+		tuleapRepositoryConnector.performQuery(taskRepository, query, collector, null,
+				new NullProgressMonitor());
+
+		assertThat(collector.getTaskData().size(), is(1));
+		assertThat(collector.getTaskData().iterator().next().getTaskId(), is(TuleapTaskIdentityUtil
+				.getTaskDataId(projectId, trackerId, artifactId)));
+	}
+
+	/**
+	 * Test the execution of a query to retrieve all the artifacts from a given tracker.
+	 */
+	@Test
+	public void testPerformQueryCustom() {
+		int projectId = 979;
+		int trackerId = 42;
+		int artifactId = 121;
+
+		final TuleapServerConfiguration tuleapServerConfiguration = new TuleapServerConfiguration(
+				"https://tuleap.net");
+
+		TuleapProjectConfiguration tuleapProjectConfiguration = new TuleapProjectConfiguration("", projectId);
+		TuleapTrackerConfiguration trackerConfiguration = new TuleapTrackerConfiguration(trackerId, null,
+				null, null, null, 0);
+		tuleapProjectConfiguration.addTracker(trackerConfiguration);
+
+		tuleapServerConfiguration.addProject(tuleapProjectConfiguration);
+
+		final TuleapArtifact tuleapArtifact = new TuleapArtifact(artifactId, trackerId, "", "", "",
+				new Date(), new Date());
+
+		final TuleapClientManager tuleapClientManager = new TuleapClientManager() {
+			@Override
+			public TuleapSoapClient getSoapClient(TaskRepository taskRepository) {
+				return new TuleapSoapClient(null, null, null, null, null) {
+					@Override
+					public List<TuleapArtifact> getArtifactsFromQuery(IRepositoryQuery query,
+							TuleapServerConfiguration serverConfiguration,
+							TuleapTrackerConfiguration tuleapTrackerConfiguration, IProgressMonitor monitor) {
+						return Lists.newArrayList(tuleapArtifact);
+					}
+				};
+			}
+		};
+
+		TuleapRepositoryConnector tuleapRepositoryConnector = new TuleapRepositoryConnector() {
+			@Override
+			public TuleapServerConfiguration getRepositoryConfiguration(TaskRepository taskRepository,
+					boolean forceRefresh, IProgressMonitor monitor) {
+				return tuleapServerConfiguration;
+			}
+
+			@Override
+			public TuleapClientManager getClientManager() {
+				return tuleapClientManager;
+			}
+		};
+
+		TaskRepository taskRepository = new TaskRepository(ITuleapConstants.CONNECTOR_KIND,
+				"https://tuleap.net");
+
+		// All from tracker
+		TuleapTaskDataCollector collector = new TuleapTaskDataCollector();
+
+		IRepositoryQuery query = new RepositoryQuery(ITuleapConstants.CONNECTOR_KIND, "");
+		query.setAttribute(ITuleapQueryConstants.QUERY_KIND, ITuleapQueryConstants.QUERY_KIND_CUSTOM);
+		query.setAttribute(ITuleapQueryConstants.QUERY_CONFIGURATION_ID, String.valueOf(trackerId));
+
+		tuleapRepositoryConnector.performQuery(taskRepository, query, collector, null,
+				new NullProgressMonitor());
+
+		assertThat(collector.getTaskData().size(), is(1));
+		assertThat(collector.getTaskData().iterator().next().getTaskId(), is(TuleapTaskIdentityUtil
+				.getTaskDataId(projectId, trackerId, artifactId)));
+	}
 }
