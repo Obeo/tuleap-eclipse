9bc8ad833488bf5b16a2493b746717c1762ff5a8
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2014-01-10 14:51:22 +0100

    Add a new test for the TuleapSoapConnector.
    Change-Id: I87436f3e2c6fe064c9b2f44babfe0b2e98f37b45

--- org.tuleap.mylyn.task.tests/src/org/tuleap/mylyn/task/internal/tests/client/soap/TuleapSoapConnectorTests.java
+++ org.tuleap.mylyn.task.tests/src/org/tuleap/mylyn/task/internal/tests/client/soap/TuleapSoapConnectorTests.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 import javax.xml.rpc.ServiceException;
 
+import org.apache.axis.AxisFault;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -303,14 +304,142 @@ public class TuleapSoapConnectorTests {
 
 			TuleapTaskId artifactIdentifier = tuleapSoapConnector.createArtifact(tuleapArtifact,
 					new NullProgressMonitor());
-			assertThat(artifactIdentifier, is(TuleapTaskId.forArtifact(projectId, configurationId,
-					elementId)));
+			assertThat(artifactIdentifier,
+					is(TuleapTaskId.forArtifact(projectId, configurationId, elementId)));
 		} catch (MalformedURLException e) {
 			fail(e.getMessage());
 		} catch (RemoteException e) {
 			fail(e.getMessage());
 		} catch (ServiceException e) {
 			fail(e.getMessage());
+		}
+	}
+
+	/**
+	 * Test the update of an artifact.
+	 */
+	@Test
+	public void testUpdateArtifact() {
+		TaskRepository taskRepository = new TaskRepository(ITuleapConstants.CONNECTOR_KIND,
+				"https://tuleap.net");
+		AuthenticationCredentials credentials = new AuthenticationCredentials("admin", "password");
+		taskRepository.setCredentials(AuthenticationType.REPOSITORY, credentials, true);
+
+		AbstractWebLocation location = new TaskRepositoryLocationFactory().createWebLocation(taskRepository);
+
+		final int artifactId = 42;
+		final int projectId = 1337;
+		final int configurationId = 666;
+		final Tracker tracker = new Tracker(configurationId, projectId, null, null, null);
+
+		final TrackerField[] trackerFields = new TrackerField[2];
+
+		int firstFieldId = 22;
+		TrackerField trackerField = new TrackerField(configurationId, firstFieldId, null, null,
+				ITuleapTrackerConstants.TYPE_ARTIFACT_LINK, null, null, new String[] {"submit", "update" });
+		trackerFields[0] = trackerField;
+
+		final int bindValueId1 = 4745;
+		final int bindValueId2 = 89432;
+		final int bindValueId3 = 7945;
+
+		int secondFieldId = 4;
+		trackerField = new TrackerField(configurationId, secondFieldId, null, null,
+				ITuleapTrackerConstants.TYPE_CB, null, null, new String[] {"submit", "update" });
+		trackerField
+				.setValues(new TrackerFieldBindValue[] {new TrackerFieldBindValue(bindValueId1, null),
+						new TrackerFieldBindValue(bindValueId2, null),
+						new TrackerFieldBindValue(bindValueId3, null), });
+		trackerFields[1] = trackerField;
+
+		TuleapReference projectRef = new TuleapReference(projectId, null);
+		TuleapReference trackerRef = new TuleapReference(configurationId, null);
+		TuleapArtifact tuleapArtifact = new TuleapArtifact(artifactId, projectRef, null, null, null, null,
+				null);
+		tuleapArtifact.setTracker(trackerRef);
+		final String firstFieldValue = "such, wow, very";
+		tuleapArtifact.addFieldValue(new LiteralFieldValue(firstFieldId, firstFieldValue));
+		tuleapArtifact.addFieldValue(new BoundFieldValue(secondFieldId, Lists.newArrayList(bindValueId2)));
+		final String newComment = "This is a new comment";
+		tuleapArtifact.setNewComment(newComment);
+
+		try {
+			final CodendiAPIPortType codendiAPIPortType = new CodendiAPIBindingStub() {
+
+				@Override
+				public Group[] getMyProjects(String sessionKey) throws RemoteException {
+					return new Group[] {new Group(projectId, null, null, null) };
+				}
+			};
+
+			final TuleapTrackerV5APIPortType tuleapTrackerV5APIPortType = new TuleapTrackerV5APIBindingStub() {
+				@Override
+				public Tracker[] getTrackerList(String sessionKey, int groupId) throws RemoteException {
+					return new Tracker[] {tracker, };
+				}
+
+				@Override
+				public TrackerField[] getTrackerFields(String sessionKey, int groupId, int trackerId)
+						throws RemoteException {
+					return trackerFields;
+				}
+
+				/**
+				 * {@inheritDoc}
+				 * 
+				 * @see org.tuleap.mylyn.task.internal.core.wsdl.soap.v2.TuleapTrackerV5APIBindingStub#updateArtifact(java.lang.String,
+				 *      int, int, int, org.tuleap.mylyn.task.internal.core.wsdl.soap.v2.ArtifactFieldValue[],
+				 *      java.lang.String, java.lang.String)
+				 */
+				@Override
+				public int updateArtifact(String sessionKey, int groupId, int trackerId, int id,
+						ArtifactFieldValue[] value, String comment, String commentFormat)
+						throws RemoteException {
+					assertThat(groupId, is(projectId));
+					assertThat(trackerId, is(configurationId));
+					assertThat(id, is(artifactId));
+					assertThat(comment, is(newComment));
+					assertThat(value.length, is(2));
+					assertThat(value[0].getField_value().getValue(), is(firstFieldValue));
+					assertThat(value[1].getField_value().getBind_value().length, is(1));
+					assertThat(value[1].getField_value().getBind_value()[0].getBind_value_id(),
+							is(bindValueId2));
+					return id;
+				}
+			};
+
+			TuleapSoapConnector tuleapSoapConnector = new TuleapSoapConnector(location) {
+				@Override
+				protected IStatus login(IProgressMonitor monitor) throws MalformedURLException,
+						ServiceException, RemoteException {
+					return Status.OK_STATUS;
+				}
+
+				@Override
+				protected IStatus logout() {
+					return Status.OK_STATUS;
+				}
+
+				@Override
+				protected TuleapTrackerV5APIPortType getTuleapTrackerV5APIPortType() {
+					return tuleapTrackerV5APIPortType;
+				}
+
+				@Override
+				protected CodendiAPIPortType getCodendiAPIPortType() {
+					return codendiAPIPortType;
+				}
+			};
+
+			tuleapSoapConnector.updateArtifact(tuleapArtifact, new NullProgressMonitor());
+		} catch (AxisFault e) {
+			fail(e.getMessage());
+		} catch (MalformedURLException e) {
+			fail(e.getMessage());
+		} catch (RemoteException e) {
+			fail(e.getMessage());
+		} catch (ServiceException e) {
+			e.printStackTrace();
 		}
 	}
 }
